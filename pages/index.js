111812f512ef61552aa38527f412688009cb5a56
cereallarceny <me@patrickcason.com>
2019-06-28 15:43:50 +0100

    Now with emotion

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
+import { css, Global } from '@emotion/core';
+import styled from '@emotion/styled';
+
 import Page from '../components/page';
 
 import patrick from '../static/patrick.jpg';
 
+const Basic = styled.div(
+  css({
+    backgroundColor: 'white',
+    color: 'cornflowerblue',
+    border: '1px solid lightgreen',
+    padding: '1rem 0.5rem'
+  })
+);
+
 export default () => (
   <Page>
-    <h1>Hello world!</h1>
+    <Global
+      styles={css`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+        }
+      `}
+    />
+    <Basic>Hello!</Basic>
     <img src={patrick} alt="Patrick Cason" />
   </Page>
 );
