8fb399bf5fad3a6fcd6e43d6307418e8f776b601
Patrick Cason <me@patrickcason.com>
2023-03-15 12:24:51 -0500

    Upgrading react-tooltip which broke at runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Tooltip from './components/Tooltip';
+import Tooltip from './Tooltip';
 
 import styles from './page.module.css';
 
